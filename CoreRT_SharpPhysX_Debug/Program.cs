357eb4e5b43d32d41902578e4b9729f1a865ad73
Alan <alanqdev@gmail.com>
2019-04-23 13:44:09 -0300

    Add project files.

---
+++ CoreRT_SharpPhysX_Debug/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreRT_SharpPhysX_Debug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
